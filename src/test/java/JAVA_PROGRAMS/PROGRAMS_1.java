8ec8a52da29db0985a5fe5a3c8503040f46d2802
GulnarPerween <65553533+GulnarPerween@users.noreply.github.com>
2025-01-22 23:17:01 +0530

    Update PROGRAMS_1.java

--- src/test/java/JAVA_PROGRAMS/PROGRAMS_1.java
+++ src/test/java/JAVA_PROGRAMS/PROGRAMS_1.java
@@ -22,6 +22,7 @@ public static void main(String[] args) {
 	                     
 	                 }
 	    System.out.println("the modified string is "+ s);   
+	//System.out.println("the modified string is "+ s);   
 	            
 	            
 	        }
